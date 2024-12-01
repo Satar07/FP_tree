bab617921ae3fce3fd28743332ce03feada2ce0d
Cyril <multya77@gmail.com>
2024-12-01 15:40:21 +0800

    Refactor data handling and support calculations in FPTree implementation

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 from anytree import *
 
 
-def read_data(file: str) -> list[list[str]]:
+class ItemWithSupport():
+    def __init__(self, name: str, support: int):
+        self.name = name
+        self.support = support
+
+    def __str__(self):
+        return f"{self.name}: {self.support}"
+
+
+def read_data(file: str) -> list[list[ItemWithSupport]]:
     data = []
     with open(file, 'r', encoding="utf-8") as f:
         for line in f:
-            data.append(line.strip().split(','))
-    return data #TODO return (data, support)
+            data.append([ItemWithSupport(s, 1)
+                        for s in line.strip().split(',')])
+    return data
 
 
-def get_every_single_support(data: list[list[str]], prev_support=None) -> dict[str, int]:
+def get_every_single_support(data: list[list[ItemWithSupport]]) -> dict[str, int]:
     support = {}
-    for i in range(len(data)):
-        for item in data[i]:
-            if item in support:
-                support[item] += 1 if prev_support is None else prev_support[i]
+    for row in data:
+        for item in row:
+            if item.name in support:
+                support[item.name] += item.support
             else:
-                support[item] = 1 if prev_support is None else prev_support[i]
+                support[item.name] = item.support
     return support
 
 
@@ -34,46 +44,71 @@ class FPNode(LightNodeMixin):
 
 class FPTree():
     root = None
-    freq_one_item = []
-    item_to_node = {}
+    m_freq_one_item = []
+    m_item_to_node = {}
+    m_min_support = 0
+    m_item_support = {}
 
-    def __init__(self, data: list[list[str]], min_support: int, prev_support=None):
-        support = get_every_single_support(data, prev_support)
-        self.freq_one_item = [
-            item for item in support if support[item] >= min_support]
-        self.freq_one_item.sort(key=lambda x: support[x], reverse=True)
+    def __init__(self, data: list[list[ItemWithSupport]], min_support: int):
+        self.m_min_support = min_support
+        self.m_item_support = get_every_single_support(data)
+        self.m_freq_one_item = [
+            item for item in self.m_item_support if self.m_item_support[item] >= min_support]
+        self.m_freq_one_item.sort(
+            key=lambda x: self.m_item_support[x], reverse=True)
 
         sorted_data = []
-        for items in data:
-            row = []
-            for item in self.freq_one_item:
-                if item in items:
-                    row.append(item)
-            sorted_data.append(row)
+        for row in data:
+            sorted_row = sorted(
+                [item for item in row if item.name in self.m_freq_one_item],
+                key=lambda x: self.m_item_support[x.name],
+                reverse=True
+            )
+            sorted_data.append(sorted_row)
 
         self.root = FPNode("root", 0)
         for row in sorted_data:
             self.insert(row, self.root)
 
-    def insert(self, items: list[str], node: FPNode):
+    def insert(self, items: list[ItemWithSupport], node: FPNode):
         if not items:
             return
         for child in node.children:
-            if child.name == items[0]:
-                child.value += 1
+            if child.name == items[0].name:
+                child.value += items[0].support
                 self.insert(items[1:], node=child)
                 return
-        new_node = FPNode(items[0], 1, parent=node)
-        if items[0] not in self.item_to_node:
-            self.item_to_node[items[0]] = [new_node]
+        new_node = FPNode(items[0].name, items[0].support, parent=node)
+        if items[0].name not in self.m_item_to_node:
+            self.m_item_to_node[items[0].name] = [new_node]
         else:
-            self.item_to_node[items[0]].append(new_node)
+            self.m_item_to_node[items[0].name].append(new_node)
         self.insert(items[1:], new_node)
 
+    def get_conditional_tree(self, item: str) -> 'FPTree':
+        data = []
+        for node in self.m_item_to_node[item]:
+            path = []
+            first_support = node.value
+            if node.parent.name == "root":
+                continue
+            node = node.parent # skip the item itself
+            while node.parent.name != "root":
+                path.append(ItemWithSupport(node.name, first_support))
+                node = node.parent
+            data.append(path)
+        return FPTree(data, self.m_min_support)
 
-def get_freq_mode(data: list[list[str]], min_support: int) -> dict[str, int]:
-    tree = FPTree(data, min_support)
-    freq_mode = {}
-    for item in tree.freq_one_item:
-        freq_mode[item] = tree.root.children[item].value
-    return freq_mode
+    def get_freq_mode(self) -> dict[str,int]:
+        freq_mode = {}
+        for item in self.m_freq_one_item[::-1]:
+            # dig the mode of item in freq_one_item_set
+            freq_mode[item]=self.m_item_support[item]
+            con_tree = self.get_conditional_tree(item)
+            sub_freq_mode  = con_tree.get_freq_mode()
+            # merge
+            for (sub_item,sub_support) in sub_freq_mode.items():
+                freq_mode[sub_item+','+item] = sub_support
+        return freq_mode
+        
+                
--- test_main.py
+++ test_main.py
@@ -1,11 +1,5 @@
 from main import *
 
-
-def test_read_data():
-    data = read_data('book.csv')
-    assert data[0] == ["薯片", "鸡蛋", "面包", "牛奶"]
-
-
 def test_get_every_single_support():
     data = read_data('book.csv')
     support = get_every_single_support(data)
@@ -18,11 +12,11 @@ def test_get_PFTree():
     tree = FPTree(data, min_support=3)
     assert tree.root.children[0].name == "薯片"
     assert tree.root.children[0].value == 7
-    assert tree.freq_one_item == ["薯片", "鸡蛋", "面包", "牛奶", "啤酒"]
+    assert tree.m_freq_one_item == ["薯片", "鸡蛋", "面包", "牛奶", "啤酒"]
 
 
 def test_get_freq_mode():
     data = read_data('book.csv')
-    freq_mode = get_freq_mode(data, min_support=3)
+    freq_mode = FPTree(data, min_support=3).get_freq_mode()
     assert freq_mode.get("薯片") == 7
-    assert freq_mode.get("薯片，鸡蛋") == 6
+    assert freq_mode.get("薯片,鸡蛋") == 6
